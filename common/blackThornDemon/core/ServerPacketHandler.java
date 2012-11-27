17a6842fd898b6bc82a57ec066cb087b9ba7c6f1
BlackThornDemon <sprinkles1435@gmail.com>
2012-11-26 22:58:48 -0500

    duplicate method, wtf?

--- common/blackThornDemon/core/ServerPacketHandler.java
+++ common/blackThornDemon/core/ServerPacketHandler.java
@@ -9,16 +9,11 @@ import cpw.mods.fml.common.network.Player;
 public class ServerPacketHandler implements IPacketHandler
 {
 	@Override 
-	public void onPacketData(NetworkManager manager, Packet250CustomPayload payload, Player player)
+	public void onPacketData(INetworkManager manager, Packet250CustomPayload payload, Player player)
 	{ 
-	DataInputStream data = new DataInputStream(new ByteArrayInputStream(payload.data));
+		DataInputStream data = new DataInputStream(new ByteArrayInputStream(payload.data));
 	}
 
-	@Override
-	public void onPacketData(INetworkManager manager,
-			Packet250CustomPayload packet, Player player) {
-		// TODO Auto-generated method stub
-		
-	}
+
 
 }
